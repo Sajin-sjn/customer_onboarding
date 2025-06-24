607510c939ddb72a95a6cedec5151d03fbfaf007
Sajin PJ <sajinsjn4433@gmail.com>
2025-06-24 13:07:33 +0530

    Add random 6-character customer ID generator service

--- src/main/java/com/acabes/bank/customer_onboarding/services/CustomerService.java
+++ src/main/java/com/acabes/bank/customer_onboarding/services/CustomerService.java
@@ -4,17 +4,37 @@ import com.acabes.bank.customer_onboarding.entities.Customer;
 import com.acabes.bank.customer_onboarding.repositories.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 public class CustomerService  {
 
     private final CustomerRepository customerRepository;
+
+
+    private static final String PREFIX = "CUST";
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int RANDOM_LENGTH = 6;
+
+    private final SecureRandom random = new SecureRandom();
+
+    public String generateCustomerId() {
+        StringBuilder sb = new StringBuilder(PREFIX);
+        for (int i = 0; i < RANDOM_LENGTH; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(index));
+        }
+        return sb.toString(); // e.g., CUSTA1B2C3
+    }
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     public Customer createCustomer(Customer customer){
-       return customerRepository.save(customer);
+        String customerId = generateCustomerId();
+        customer.setCustomerId(customerId);
+        return customerRepository.save(customer);
     }
 
     public Customer getCustomer(String id){
